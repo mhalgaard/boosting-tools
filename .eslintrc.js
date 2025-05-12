360acda657cd0b369ff390312a06b3ec39a63926
Mikkel Halgaard Kjær <mhalgaard94@gmail.com>
2025-05-12 21:35:54 +0200

    tanstack router added and prettier settup

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    // Add custom ESLint rules here
+  },
+};
