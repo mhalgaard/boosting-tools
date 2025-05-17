2873f1837506d69d62026aef3bd25203e4f148f9
Mikkel Halgaard Kjær <mhalgaard94@gmail.com>
2025-05-17 16:04:48 +0200

    Updates

---
+++ assets/apply-BOvIEYx-.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DvrbsM1K.js";const o=function(){return e.jsx("div",{className:"flex flex-row justify-center items-center h-full w-full",children:e.jsx("div",{className:"text-9xl",children:"You were rejected :("})})};export{o as component};
--- assets/index-Cs9vRNBy.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-D2hYNN4A.js";const t=function(){return e.jsx("div",{className:"p-2",children:e.jsx("h3",{children:"Welcome Home!"})})};export{t as component};
